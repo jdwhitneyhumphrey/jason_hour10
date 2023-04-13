917d468cf9160de29b50610734ee6eb8fa6a9689
jdwhitneyhumphrey <124301860+jdwhitneyhumphrey@users.noreply.github.com>
2023-04-13 12:04:41 -0700

    fixes

--- Assets/GameCritic.txt/GameCritic.txt.cs
+++ Assets/GameCritic.txt/GameCritic.txt.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+
+    private bool isGameOver = true;
+
     void Update()
-    }
+    {
      
         if (isGameOver){
+            
             print("Enhancement 1: Although it does provide a challenge, it seems a bit lacking as all you got are just chaos balls to interfere but nothing else, perhaps add an enemy or two. Enchancement 2: Seems a bit short as there's just four goals, maybe add more to keep the player engaged longer. Enhancement 3: There's not really any hazards of any kind or obstacles to navigate, consider adding those to add a challenge. Enhancement 4: The Chaos balls and colored balls are difficult to get to due to their speed, consider slowing them down a bit whenever possible.");
-        
+
         }
-    { 
+        
+    }
 }
