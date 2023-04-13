6820e942dffea41603daefdbf977d058de1acc35
jdwhitneyhumphrey <124301860+jdwhitneyhumphrey@users.noreply.github.com>
2023-04-13 12:18:56 -0700

    Update GameCritic.txt.cs

--- Assets/GameCritic.txt/GameCritic.txt.cs
+++ Assets/GameCritic.txt/GameCritic.txt.cs
@@ -12,7 +12,7 @@ public class NewBehaviourScript : MonoBehaviour
      
         if (isGameOver){
             
-            print("Enhancement 1: Although it does provide a challenge, it seems a bit lacking as all you got are just chaos balls to interfere but nothing else, perhaps add an enemy or two. Enchancement 2: Seems a bit short as there's just four goals, maybe add more to keep the player engaged longer. Enhancement 3: There's not really any hazards of any kind or obstacles to navigate, consider adding those to add a challenge. Enhancement 4: The Chaos balls and colored balls are difficult to get to due to their speed, consider slowing them down a bit whenever possible.");
+            print("Enhancement 1: Although it does provide a challenge, it seems a bit lacking as all you got are just at least four chaos balls to interfere but nothing else, perhaps add an enemy or two. Enchancement 2: Seems a bit short as there's just four goals, maybe add more to keep the player engaged longer. Enhancement 3: There's not really any hazards of any kind or obstacles to navigate, consider adding those to add a challenge. Enhancement 4: The Chaos balls and colored balls are difficult to get to due to their speed, consider slowing them down a bit whenever possible.");
 
         }
         
